6adb506c87baf68fe9c37c7c22ad73f7dff8ba0d
TomaszUrban99 <tomasz.urban.116@gmail.com>
2023-03-30 10:07:08 +0200

    Created and tested function for receiveng file path from http request.

--- inc/http_request.h
+++ inc/http_request.h
@@ -1,11 +1,13 @@
 #ifndef HTTP_REQUEST_HH
 #define HTTP_REQUEST_HH
 
+#include <stdio.h>
 #include <string.h>
 
 /* Enumeration type for different types of requests */
 enum requestTypes
 {
+    NOT_RECOGNIZED,
     GET,
     POST
 };
@@ -17,6 +19,8 @@ struct http_request
     double httpVersion;
 };
 
-struct http_request *receiveRequest(struct http_request *newRequest, char *incomingRequest);
+struct http_request *receiveRequest(struct http_request *newRequest,
+                                    char *incomingRequest);
 
+char *receiveRequestPath(char *incomingRequest);
 #endif
--- src/http_request.c
+++ src/http_request.c
@@ -1,16 +1,45 @@
 #include "http_request.h"
 
-struct http_request *receiveRequest(struct http_request *newRequest, char *incomingRequest)
+struct http_request *receiveRequest(struct http_request *newRequest,
+                                    char *incomingRequest)
 {
+    char *tmpChar = incomingRequest;
+
+    /* Get the type of http request */
     if (strstr(incomingRequest, "GET") != NULL)
-    {
         newRequest->requestType = GET;
-    }
 
     if (strstr(incomingRequest, "POST") != NULL)
-    {
         newRequest->requestType = POST;
+    else
+    {
+        fprintf(stderr, "%s", "HTTP request has not been recognized\n");
+        return NULL;
     }
 
+    printf("%s%s\n", "PATH: ", receiveRequestPath(tmpChar));
+
     return newRequest;
+}
+
+char *receiveRequestPath(char *incomingRequest)
+{
+    int size = 0;
+    char *tmpChar = strstr(incomingRequest, "/");
+
+    if (tmpChar == NULL)
+    {
+        fprintf(stderr, "%s\n", "Path request has not been found\n");
+        return NULL;
+    }
+
+    incomingRequest = tmpChar;
+
+    while (*incomingRequest != ' ')
+    {
+        incomingRequest++;
+        size++;
+    }
+
+    return strncpy(incomingRequest - size, tmpChar, size);
 }
